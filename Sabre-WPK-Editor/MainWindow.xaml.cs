339b8b3e584ff6aaab09a2fb363ccbbe176c3771
Crauzer <thenatokenproject@gmail.com>
2016-11-01 10:16:12 +0100

    Add project files.

---
+++ Sabre-WPK-Editor/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Sabre_WPK_Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WPKFile wpk;
+        private Microsoft.Win32.OpenFileDialog ofd;
+        private Microsoft.Win32.OpenFileDialog ofd2;
+        private Microsoft.Win32.OpenFileDialog ofd3;
+        public MainWindow()
+        {
+            Directory.CreateDirectory(Environment.CurrentDirectory + "/" + "WEMFiles" + "/");
+            Directory.CreateDirectory(Environment.CurrentDirectory + "/" + "WPKFiles" + "/");
+            InitializeComponent();
+        }
+
+        private void buttonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Filter = "WPK files (*.wpk)|*.wpk";
+            if(ofd.ShowDialog() == true)
+            {
+                buttonAutofill.IsEnabled = true;
+                buttonExtract.IsEnabled = true;
+                buttonExtractAll.IsEnabled = true;
+                buttonSave.IsEnabled = true;
+                buttonImport.IsEnabled = true;
+                listAudioFiles.Items.Clear();
+                wpk = new WPKFile(ofd.FileName);
+                foreach(var a in wpk.AudioFiles)
+                {
+                    listAudioFiles.Items.Add(a.Name);
+                }
+            }
+        }
+
+        private void buttonExtractAll_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(var a in wpk.AudioFiles)
+            {
+                File.WriteAllBytes(Environment.CurrentDirectory + "/WEMFiles/" + a.Name, a.Data);
+            }
+        }
+
+        private void buttonExtract_Click(object sender, RoutedEventArgs e)
+        {
+            string selected = listAudioFiles.Items[listAudioFiles.SelectedIndex].ToString();
+            var s = wpk.AudioFiles.Find(x => x.Name == selected);
+            File.WriteAllBytes(Environment.CurrentDirectory + "/WEMFiles/" + s.Name, s.Data);
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            BinaryWriter bw = new BinaryWriter(File.OpenWrite(ofd.FileName));
+
+            bw.Write(wpk.header.Magic.ToCharArray());
+            bw.Write(wpk.header.Version);
+            bw.Write(wpk.header.AudioCount);
+            foreach(var m in wpk.AudioFiles)
+            {
+                bw.Write(m.mtOff);
+            }
+            uint LastOffset = wpk.AudioFiles[0].DataOffset;
+            foreach(var a in wpk.AudioFiles)
+            {
+                bw.BaseStream.Seek(a.mtOff, SeekOrigin.Begin);
+                a.DataOffset = LastOffset;
+                bw.Write(a.DataOffset);
+                bw.Write(a.DataSize);
+                bw.Write(a.NameLength);
+                bw.Write(a.tempName);
+                LastOffset += (uint)a.Data.Length;
+            }
+            foreach(var d in wpk.AudioFiles)
+            {
+                bw.BaseStream.Seek(d.DataOffset, SeekOrigin.Begin);
+                bw.Write(d.Data);
+            }
+        }
+
+        private void buttonImport_Click(object sender, RoutedEventArgs e)
+        {
+            BinaryReader br;
+            byte[] Data;
+            ofd2 = new Microsoft.Win32.OpenFileDialog();
+            ofd2.Filter = "WEM files (*.wem)|*.wem";
+            if (ofd2.ShowDialog() == true)
+            {
+                br = new BinaryReader(File.Open(ofd2.FileName, FileMode.Open));
+                Data = br.ReadBytes((int)br.BaseStream.Length);
+                string selected = listAudioFiles.Items[listAudioFiles.SelectedIndex].ToString();
+                var s = wpk.AudioFiles.Find(x => x.Name == selected);
+                s.Data = Data;
+                s.DataSize = (uint)Data.Length;
+            }
+        }
+
+        private void buttonAutofill_Click(object sender, RoutedEventArgs e)
+        {
+            string[] files;
+            byte[] Data;    
+            int i = 0;
+            ofd3 = new Microsoft.Win32.OpenFileDialog();
+            BinaryReader br;
+            ofd3.Multiselect = true;
+            ofd3.Filter = "WEM files (*.wem)|*.wem";
+            if (ofd3.ShowDialog() == true)
+            {
+                files = ofd3.FileNames;
+                foreach (var a in wpk.AudioFiles)
+                {
+                    br = new BinaryReader(File.Open(files[i], FileMode.Open));
+                    Data = br.ReadBytes((int)br.BaseStream.Length);
+                    a.Data = Data;
+                    a.DataSize = (uint)Data.Length;
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ Sabre-WPK-Editor/WPKFile.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Sabre_WPK_Editor
+{
+    class WPKFile
+    {
+        public BinaryReader br;
+        public Header header;
+        public List<AudioFile> AudioFiles = new List<AudioFile>();
+        public uint LastOffset = 0;
+        public WPKFile(string fileLocation)
+        {
+            br = new BinaryReader(File.Open(fileLocation, FileMode.Open));
+            header = new Header(br);
+            for(int i = 0; i < header.AudioCount; i++)
+            {
+                AudioFiles.Add(new AudioFile(br.ReadUInt32()));
+            }
+            LastOffset = AudioFiles[0].DataOffset;
+            foreach(var a in AudioFiles)
+            {
+                br.BaseStream.Seek(a.mtOff, SeekOrigin.Begin);
+                a.DataOffset = br.ReadUInt32();
+                a.DataSize = br.ReadUInt32();
+                a.NameLength = br.ReadUInt32();
+                a.tempName = br.ReadChars((int)a.NameLength * 2);
+                a.Name = GetWPKName(a.tempName);
+            }
+            foreach(var a in AudioFiles)
+            {
+                br.BaseStream.Seek(a.DataOffset, SeekOrigin.Begin);
+                a.Data = br.ReadBytes((int)a.DataSize);
+            }
+            br.Dispose();
+            br.Close();
+        }
+        public class Header
+        {
+            public string Magic;
+            public UInt32 Version;
+            public UInt32 AudioCount;
+            public Header(BinaryReader br)
+            {
+                Magic = Encoding.ASCII.GetString(br.ReadBytes(4));
+                Version = br.ReadUInt32();
+                AudioCount = br.ReadUInt32();
+            }
+        }
+        public class AudioFile
+        {
+            public UInt32 mtOff;
+            public UInt32 DataOffset;
+            public UInt32 DataSize;
+            public UInt32 NameLength;
+            public char[] tempName;
+            public string Name;
+            public byte[] Data;
+            public AudioFile(UInt32 metaOffset)
+            {
+                mtOff = metaOffset;
+            }
+            public static void ExtractFile(string fileLocation, string extractPath, UInt32 DataOffset, UInt32 DataSize)
+            {
+                using (BinaryReader br = new BinaryReader(new FileStream(fileLocation, FileMode.Open)))
+                { 
+                    br.BaseStream.Seek(DataOffset, SeekOrigin.Begin);
+                    File.WriteAllBytes(extractPath, br.ReadBytes((int)DataSize));
+                    br.Dispose();
+                    br.Close();
+                }
+            }
+        }
+        public static string GetWPKName(char[] tempName)
+        {
+            string name = "";
+            foreach(char c in tempName)
+            {
+                if(c != '\0')
+                {
+                    name += c;
+                }
+            }
+            return name;
+        }
+    }
+}
